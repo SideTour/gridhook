ec80ce0f55bdfd9c4bd79d9b98a8b17568eb1234
Scott Werner <stwerner@vt.edu>
2013-08-20 11:15:02 -0400

    Change way gem files are loaded in test

--- test/helper.rb
+++ test/helper.rb
@@ -2,11 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 # require File.expand_path '../dummy/config/environment', __FILE__
 
 require 'minitest/autorun'
-
-unless Object.const_defined? 'Gridhook'
-  $:.unshift File.expand_path('../../lib', __FILE__)
-  require 'gridhook'
-end
+require File.join(File.dirname(__FILE__), *%w{.. lib gridhook})
 
 class TestCase < MiniTest::Unit::TestCase
   def self.test(name, &block)
